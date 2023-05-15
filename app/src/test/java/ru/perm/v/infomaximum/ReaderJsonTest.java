ec895241b8b749644b9620e522beb5a772fa6906
Черепахин Василий <vasi.che@gmail.com>
2023-05-15 13:17:57 +0500

    simple reader json file (for example)

--- app/src/test/java/ru/perm/v/infomaximum/ReaderJsonTest.java
+++ app/src/test/java/ru/perm/v/infomaximum/ReaderJsonTest.java
@@ -4,16 +4,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.StringReader;
 
 public class ReaderJsonTest {
+    // Simple reader JSON
     @Test
-    public void readFromString() {
+    public void readSimpleObjFromJsonString() {
         String replyString = "{\"msg\": \"MSG\", \"pass\": true}";
         JsonReader jsonReader = Json.createReader(new StringReader(replyString));
         JsonObject reply = jsonReader.readObject();
@@ -21,28 +19,5 @@ public class ReaderJsonTest {
         String msg = reply.getString("msg");
         Assert.assertEquals("MSG", msg);
         Assert.assertTrue(pass);
-    }
-
-    @Test
-    public void existFile() throws FileNotFoundException {
-        FileReader file = new FileReader("src/test/resources/out.json");
-        Assert.assertNotNull(file);
-    }
-
-    @Test
-    public void readArrayFromFile() throws FileNotFoundException {
-        JsonReader jsonReader = Json.createReader(new FileReader("src/test/resources/out.json"));
-        JsonArray products = jsonReader.readArray();
-        products.getValuesAs(jsonValue -> jsonValue.asJsonObject());
-        Assert.assertEquals(4, products.size());
-
-        JsonObject p = products.get(0).asJsonObject();
-        Product product = new Product();
-        product.setGrp(p.getString("grp"));
-        product.setType(p.getString("type"));
-        product.setNum(p.getInt("num"));
-        product.setWeight(p.asJsonObject().getInt("weight"));
-
-        Assert.assertEquals(new Product("grp1", "type1", 1, 111), product);
     }
 }
