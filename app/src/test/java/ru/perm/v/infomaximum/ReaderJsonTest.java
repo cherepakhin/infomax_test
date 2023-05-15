d658e333c62dbba3cb1556bca7db6f8e3df709df
Черепахин Василий <vasi.che@gmail.com>
2023-05-15 11:48:45 +0500

    ReaderJsonTest.existFile

--- app/src/test/java/ru/perm/v/infomaximum/ReaderJsonTest.java
+++ app/src/test/java/ru/perm/v/infomaximum/ReaderJsonTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.StringReader;
 
 public class ReaderJsonTest {
@@ -13,6 +15,22 @@ public class ReaderJsonTest {
     public void readFromString() {
         String replyString = "{\"msg\": \"MSG\", \"pass\": true}";
         JsonReader jsonReader = Json.createReader(new StringReader(replyString));
+        JsonObject reply = jsonReader.readObject();
+        Boolean pass = reply.getBoolean("pass");
+        String msg = reply.getString("msg");
+        Assert.assertEquals("MSG", msg);
+        Assert.assertTrue(pass);
+    }
+
+    @Test
+    public void existFile() throws FileNotFoundException {
+        FileReader file = new FileReader("src/test/resources/out.json");
+        Assert.assertNotNull(file);
+    }
+
+    @Test
+    public void readFromFile() throws FileNotFoundException {
+        JsonReader jsonReader = Json.createReader(new FileReader("src/test/resources/out.json"));
         JsonObject reply = jsonReader.readObject();
         Boolean pass = reply.getBoolean("pass");
         String msg = reply.getString("msg");
