ab7f8a2c6cadd529f019e3a1e27e3f739288f1d8
Черепахин Василий <vasi.che@gmail.com>
2023-05-15 12:01:53 +0500

    add ReaderJsonTest.java

--- app/src/test/java/ru/perm/v/infomaximum/ReaderJsonTest.java
+++ app/src/test/java/ru/perm/v/infomaximum/ReaderJsonTest.java
@@ -3,12 +3,14 @@ package ru.perm.v.infomaximum;
 import org.junit.Assert;
 import org.junit.Test;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
+import javax.json.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.StringReader;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class ReaderJsonTest {
     @Test
@@ -29,12 +31,9 @@ public class ReaderJsonTest {
     }
 
     @Test
-    public void readFromFile() throws FileNotFoundException {
+    public void readArrayFromFile() throws FileNotFoundException {
         JsonReader jsonReader = Json.createReader(new FileReader("src/test/resources/out.json"));
-        JsonObject reply = jsonReader.readObject();
-        Boolean pass = reply.getBoolean("pass");
-        String msg = reply.getString("msg");
-        Assert.assertEquals("MSG", msg);
-        Assert.assertTrue(pass);
+        JsonArray products = jsonReader.readArray();
+        Assert.assertEquals(4, products.size());
     }
 }
