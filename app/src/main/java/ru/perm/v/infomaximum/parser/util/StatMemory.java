d37d0a634abbe959686c4c632a5bda38c3e71804
Черепахин Василий <vasi.che@gmail.com>
2023-05-20 14:23:19 +0500

    add StatMemory.java

---
+++ app/src/main/java/ru/perm/v/infomaximum/parser/util/StatMemory.java
@@ -0,0 +1,19 @@
+package ru.perm.v.infomaximum.parser.util;
+
+public class StatMemory {
+    public static long getMaxMemory() {
+        return Runtime.getRuntime().maxMemory();
+    }
+
+    public static long getUsedMemory() {
+        return getMaxMemory() - getFreeMemory();
+    }
+
+    public static long getTotalMemory() {
+        return Runtime.getRuntime().totalMemory();
+    }
+
+    public static long getFreeMemory() {
+        return Runtime.getRuntime().freeMemory();
+    }
+}
